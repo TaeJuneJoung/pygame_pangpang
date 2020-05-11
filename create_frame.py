32e70b2bce413bdd0d64e15811e512c635115214
TaeJuneJoung <jtj0525@gmail.com>
2020-05-12 08:33:14 +0900

    Init

---
+++ create_frame.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init() #초기화(필수)
+
+screen_width = 480
+screen_height = 640
+
+pygame.display.set_mode((screen_width, screen_height)) #화면 크기 설정
+
+pygame.display.set_caption('Nano Game') #화면 타이틀
---
+++ readMe.md
@@ -0,0 +1,6 @@
+# PangPang
+
+![Origin Pic](./docs/assets/pangpang.png)
+
+오락실 게임 '팡팡'을 이용하여 머신러닝 적용해보자.
+
